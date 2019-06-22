e11ce02ebe9d37bbf3191e05ba102dba683ecd8e
bomibear <bomibearly@gmail.com>
2019-06-22 11:50:59 -0700

    nuggets dissappear and reappear

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+'use strict';
+
 var board = document.getElementById('board');
 var ctx = board.getContext('2d');
 var gold = new Image();
+gold.src = 'gold.png';
+
 //image needs to load before it can be drawn
 gold.onload = function() {
   ctx.drawImage(gold,randomX(), randomY());
 };
 
-gold.src = 'gold.png';
+var timer = setTimeout(function countdown() {
+  ctx.clearRect(0, 0, 900,500);
+  ctx.drawImage(gold,randomX(), randomY());
+  timer = setTimeout(countdown, 3000);
+}, 3000);
 
 function randomX() {
   return Math.ceil(Math.random() * (900 - 200));
